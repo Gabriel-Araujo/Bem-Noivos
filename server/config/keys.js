4c3aa6fc1069ed3012f3b8744e57100667c051fe
Gabriel-Araujo <gabrielaraujo182@gmail.com>
2018-07-02 22:10:02 -0400

    remove comments

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,7 +1,1 @@
 module.exports = (process.env.NODE_ENV === 'production') ? require('./keys_prod') : require('./keys_dev');
-/*
-module.exports = {
-  mongoURI: 'mongodb://bnadmin:bnadmin182@ds217351.mlab.com:17351/bemnoivos',
-  secretJWT: 'secret',
-};
-*/
